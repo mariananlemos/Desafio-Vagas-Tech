a3a9ca6837700f43f0607d3083cb466438caf47b
Mariana Lemos <mariananlemos@users.noreply.github.com>
2026-09-11 09:46:37 -0300

    feat: adiciona tabela CANDIDATAS no DDL e no banco de dados

--- DDL_Criacao.sql
+++ DDL_Criacao.sql
@@ -1,1 +1,23 @@
--- pessoa 1, 2 e 3
+-- ==========================================================
+-- 1: Tabela de Origem 1 (VAGAS)
+-- ==========================================================
+CREATE TABLE VAGAS (
+    ID_VAGA INT NOT NULL,
+    TITULO VARCHAR(100) NOT NULL,
+    EMPRESA VARCHAR(100) NOT NULL,
+    SALARIO DECIMAL NOT NULL,
+    PRIMARY KEY (ID_VAGA)
+);
+
+-- ==========================================================
+-- 2: Tabela de Origem 2 (CANDIDATAS)
+-- ==========================================================
+CREATE TABLE CANDIDATAS (
+    ID_CANDIDATA INT NOT NULL,
+    NOME VARCHAR(100) NOT NULL,
+    EMAIL VARCHAR(100) NOT NULL,
+    PRIMARY KEY (ID_CANDIDATA)
+);
+
+-- 3: Tabela Associativa N:N (CANDIDATURAS)
+-- ==========================================================
